fd234e079f4ca60a5e0b92cdde0988f29815a895
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-09-05 14:04:28 +0700

    feat: initialize wwebjs connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, Bambot!");
+import { Client, LocalAuth } from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+    ],
+  },
+});
+
+client.on("qr", (qr: string) => {
+  console.log("QR Received");
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Whatsapp client is ready!");
+});
+
+client.initialize();
