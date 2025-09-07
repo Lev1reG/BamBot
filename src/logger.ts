2da6f9325cdbde8b07986635bc05ec23f8115570
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-09-07 12:37:54 +0700

    feat: create logger configuration

---
+++ src/logger.ts
@@ -0,0 +1,43 @@
+import path from "path";
+import winston from "winston";
+
+const logDir = path.join(process.cwd(), "logs");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: path.join(logDir, "error.log"),
+      level: "error",
+      maxsize: 5242880, // 5MB
+      maxFiles: 5,
+    }),
+
+    new winston.transports.File({
+      filename: path.join(logDir, "combined.log"),
+      maxsize: 5242880, // 5MB
+      maxFiles: 5,
+    }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(({ timestamp, level, message, ...meta }) => {
+          let msg = `${timestamp} [${level}]: ${message} `;
+          if (Object.keys(meta).length > 0) {
+            msg += `${JSON.stringify(meta)}`;
+          }
+          return msg;
+        })
+      ),
+    }),
+  ],
+});
+
+export default logger;
